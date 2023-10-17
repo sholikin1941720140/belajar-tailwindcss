3cab07a68ed008724eeea4e30bd770bbb4bc82d5
Sholikin <1941720140@student.polinema.ac.id>
2023-10-17 13:29:27 +0700

    tailwind and image tapi error hangdeh

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Landing;
+use Illuminate\Http\Request;
+
+class LandingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('layouts.app');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Landing $landing)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Landing $landing)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Landing $landing)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Landing $landing)
+    {
+        //
+    }
+}
